b3594982a7d19af21b8feb5dc2ac2cf3d587af0f
piptarou <piptarou@gmail.com>
2024-12-02 22:13:13 -0400

    Fixed some formatting

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
