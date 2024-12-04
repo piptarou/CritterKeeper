0397733fe7987f83588142850b97c7d9aff4e5c9
piptarou <piptarou@gmail.com>
2024-12-04 15:28:42 -0400

    Moved Critter model and created User model

---
+++ src/models/Critter.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const critterSchema = new mongoose.Schema({
+  case_number: { type: String, required: true, unique: true },
+  rescue_date: { type: Date, required: true },
+  rescue_role: { type: String, required: true },
+  critter_count: { type: Number, required: true },
+  animal_type: { type: String, required: true },
+  animal_age: { type: String, required: true },
+  conservation_status: { type: String, required: true },
+  original_location: { type: String, required: true },
+  km_driven: { type: Number, required: true },
+  volunteer_notes: { type: String, required: false }
+});
+
+module.exports = mongoose.model('Critter', critterSchema, 'critters');
